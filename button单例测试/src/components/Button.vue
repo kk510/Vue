f99e7ad18be08b1151419f32692dd857c46443ba
kk <895146260@qq.com>
2020-09-12 21:33:31 +0800

    测试内容

---
+++ button单例测试/src/components/Button.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <button data-testid="btn" :disabled="disabled" @click="handleClick">
+      <slot>kkb</slot>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  methods: {
+    handleClick() {
+      this.$emit("click");
+      console.log("123")
+
+    },
+  },
+};
+</script>
+
+<style scoped></style>
